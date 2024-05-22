98e38257e0605a7ca1263324aa38b3c41ccee5dc
Wessley <wessleynyakaz@gmail.com>
2024-05-22 11:49:41 +0200

    (chore) removed seed

--- seed/reset.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from './prisma'
-
-export async function resetDb() {
-    await prisma.student.deleteMany()
-    await prisma.assignment.deleteMany()
-    await prisma.resource.deleteMany()
-    await prisma.parent.deleteMany()
-}
